ce8785c6f94f34708af838f02d46b4e8cb54ef95
Reem Degais <reem.degais.33@gmail.com>
2025-11-03 16:39:36 -0800

    create joins_queries.sql README.md

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,21 @@
+-- a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+
+SELECT users.name, bookings.name
+FROM bookings
+INNER JOIN users
+on booking.user_id = users.id;
+
+-- a query using aLEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
+
+SELECT properties.name, reviews.comments
+FROM properties
+LEFT JOIN reviews
+on properties.id = reviews.properties_id;
+
+-- a query using a FULL OUTER JOIN to retrieve all users and all bookings,
+-- even if the user has no booking or a booking is not linked to a user.
+
+SELECT users.name, bookings.name
+FROM users
+FULL OUTER JOIN bookings
+on users.id = booking.user_id;
